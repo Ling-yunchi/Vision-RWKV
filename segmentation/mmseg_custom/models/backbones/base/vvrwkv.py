ab2adb5c970463d2ce1ee35545d689c60e15b930
Ling-yunchi <wr020210@gmail.com>
2024-10-24 15:29:12 +0800

    update

--- segmentation/mmseg_custom/models/backbones/base/vvrwkv.py
+++ segmentation/mmseg_custom/models/backbones/base/vvrwkv.py
@@ -1,21 +1,25 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from typing import Sequence
-import warnings
-import math
-
+# Copyright (c) Shanghai AI Lab. All rights reserved.
 import logging
+import math
+from typing import Sequence
+
 import torch
 import torch.nn as nn
-from torch.nn import functional as F
 import torch.utils.checkpoint as cp
-
-from mmcv.runner import load_checkpoint
-from mmcv.runner.base_module import BaseModule, ModuleList
+from einops import rearrange
+from mmcls.models.backbones.base_backbone import BaseBackbone
+from mmcls.models.builder import BACKBONES
+from mmcls.models.utils import resize_pos_embed
 from mmcv.cnn.bricks.transformer import PatchEmbed
-from mmseg.models.builder import BACKBONES
-from mmseg.utils import get_root_logger
-from mmseg_custom.models.utils import resize_pos_embed, DropPath
+from mmcv.runner.base_module import BaseModule, ModuleList
+from torch.nn import functional as F
+
+from mmseg_custom.models.backbones.scan import s_hw, s_wh, s_rhw, s_hrw, s_wrh, s_rwh, sr_hw, sr_wh, sr_rhw, sr_hrw, \
+    sr_rwh, sr_wrh
 from mmseg_custom.models.backbones.base.wkv import RUN_CUDA
+from mmseg_custom.models.utils import DropPath
+
+logger = logging.getLogger(__name__)
 
 
 def q_shift(input, shift_pixel=1, gamma=1 / 4, patch_resolution=None):
@@ -36,19 +40,128 @@ def q_shift(input, shift_pixel=1, gamma=1 / 4, patch_resolution=None):
     return output.flatten(2).transpose(1, 2)
 
 
+class RevIN(nn.Module):
+    def __init__(self, num_features: int, eps=1e-5, affine=True):
+        """
+        :param num_features: the number of features or channels
+        :param eps: a value added for numerical stability
+        :param affine: if True, RevIN has learnable affine parameters
+        """
+        super(RevIN, self).__init__()
+
+        self.num_features = num_features
+        self.eps = eps
+        self.affine = affine
+
+        if self.affine:
+            # initialize RevIN params: (C,)
+            self.affine_weight = nn.Parameter(torch.ones(self.num_features))
+            self.affine_bias = nn.Parameter(torch.zeros(self.num_features))
+
+    def forward(self, x, norm: bool):
+        h, w = x.shape[-2:]
+        x = rearrange(x, "b c h w -> b (h w) c")
+        if norm:
+            self._get_statistics(x)
+            x = self._normalize(x)
+        else:
+            x = self._denormalize(x)
+        x = rearrange(x, "b (h w) c -> b c h w", h=h, w=w)
+        return x
+
+    def _get_statistics(self, x):
+        dim2reduce = tuple(range(1, x.ndim - 1))
+        self.mean = torch.mean(x, dim=dim2reduce, keepdim=True).detach()
+        self.stdev = torch.sqrt(
+            torch.var(x, dim=dim2reduce, keepdim=True, unbiased=False) + self.eps
+        ).detach()
+
+    def _normalize(self, x):
+        x = x - self.mean
+        x = x / self.stdev
+        if self.affine:
+            x = x * self.affine_weight
+            x = x + self.affine_bias
+        return x
+
+    def _denormalize(self, x):
+        if self.affine:
+            x = x - self.affine_bias
+            x = x / (self.affine_weight + self.eps * self.eps)
+        x = x * self.stdev
+        x = x + self.mean
+        return x
+
+
+class DenseMoE(nn.Module):
+    def __init__(
+            self,
+            in_channels,
+            drop=0.1,
+            revin_affine=True,
+    ):
+        """
+        Dense MoE for Image
+        :param in_channels: input image channels
+        :param experts: num of expert, each expert input shape equal to output shape
+        :param drop: drop rate
+        :param revin_affine: use revin learnable param
+        """
+        super(DenseMoE, self).__init__()
+        self.drop = drop
+        self.revin_affine = revin_affine
+
+        self.gate = nn.Conv2d(in_channels, self.num_experts, 1)
+
+        self.dropout = nn.Dropout2d(drop)
+        self.rev = RevIN(in_channels, affine=revin_affine)
+
+    def forward(self, x):
+        x = self.rev(x, True)
+        x = self.dropout(x)  # (B, C, H, W)
+
+        score = F.softmax(self.gate(x), dim=1)  # (B, E, H, W)
+        score = score.unsqueeze(1)  # (B, 1, E, H, W)
+
+        scan_func = [s_hw, s_wh, s_rhw, s_hrw, s_rwh, s_wrh]
+        re_scan_func = [sr_hw, sr_wh, sr_rhw, sr_hrw, sr_rwh, sr_wrh]
+
+        expert_outputs = torch.stack(
+            [self.experts[i](x) for i in range(self.num_experts)], dim=2
+        )  # (B, C, E, H, W)
+
+        prediction = torch.sum(expert_outputs * score, dim=2)  # [B, C, H, W]
+
+        prediction = self.rev(prediction, False)
+
+        return prediction
+
+
 class VRWKV_SpatialMix(BaseModule):
     def __init__(self, n_embd, n_layer, layer_id, shift_mode='q_shift',
                  channel_gamma=1 / 4, shift_pixel=1, init_mode='fancy',
-                 key_norm=False):
+                 key_norm=False, drop=0.1, revin_affine=True, with_cp=False):
         super().__init__()
         self.layer_id = layer_id
         self.n_layer = n_layer
         self.n_embd = n_embd
         self.device = None
-        attn_sz = n_embd
-        self._init_weights(init_mode)
+        self.attn_sz = n_embd
         self.shift_pixel = shift_pixel
         self.shift_mode = shift_mode
+
+        # MoE System
+        self.drop = drop
+        self.revin_affine = revin_affine
+        self.num_experts = 4
+
+        self.gate = nn.Conv2d(n_embd, self.num_experts, 1)
+
+        self.dropout = nn.Dropout2d(drop)
+        self.rev = RevIN(n_embd, affine=revin_affine)
+
+        self._init_weights(init_mode)
+
         if shift_pixel > 0:
             self.shift_func = eval(shift_mode)
             self.channel_gamma = channel_gamma
@@ -57,34 +170,37 @@ class VRWKV_SpatialMix(BaseModule):
             self.spatial_mix_v = None
             self.spatial_mix_r = None
 
-        self.key = nn.Linear(n_embd, attn_sz, bias=False)
-        self.value = nn.Linear(n_embd, attn_sz, bias=False)
-        self.receptance = nn.Linear(n_embd, attn_sz, bias=False)
+        self.key = nn.Linear(n_embd, self.attn_sz, bias=False)
+        self.value = nn.Linear(n_embd, self.attn_sz, bias=False)
+        self.receptance = nn.Linear(n_embd, self.attn_sz * self.num_experts, bias=False)
         if key_norm:
-            self.key_norm = nn.LayerNorm(attn_sz)
+            self.key_norm = nn.LayerNorm(n_embd * self.num_experts)
         else:
             self.key_norm = None
-        self.output = nn.Linear(attn_sz, n_embd, bias=False)
+        self.output = nn.Linear(self.attn_sz, n_embd, bias=False)
 
         self.key.scale_init = 0
         self.receptance.scale_init = 0
         self.output.scale_init = 0
 
+        self.with_cp = with_cp
+
     def _init_weights(self, init_mode):
+        multi_dim = self.n_embd * self.num_experts
         if init_mode == 'fancy':
             with torch.no_grad():  # fancy init
                 ratio_0_to_1 = (self.layer_id / (self.n_layer - 1))  # 0 to 1
                 ratio_1_to_almost0 = (1.0 - (self.layer_id / self.n_layer))  # 1 to ~0
 
                 # fancy time_decay
-                decay_speed = torch.ones(self.n_embd)
-                for h in range(self.n_embd):
-                    decay_speed[h] = -5 + 8 * (h / (self.n_embd - 1)) ** (0.7 + 1.3 * ratio_0_to_1)
+                decay_speed = torch.ones(multi_dim)
+                for h in range(multi_dim):
+                    decay_speed[h] = -5 + 8 * (h / (multi_dim - 1)) ** (0.7 + 1.3 * ratio_0_to_1)
                 self.spatial_decay = nn.Parameter(decay_speed)
 
                 # fancy time_first
-                zigzag = (torch.tensor([(i + 1) % 3 - 1 for i in range(self.n_embd)]) * 0.5)
-                self.spatial_first = nn.Parameter(torch.ones(self.n_embd) * math.log(0.3) + zigzag)
+                zigzag = (torch.tensor([(i + 1) % 3 - 1 for i in range(multi_dim)]) * 0.5)
+                self.spatial_first = nn.Parameter(torch.ones(multi_dim) * math.log(0.3) + zigzag)
 
                 # fancy time_mix
                 x = torch.ones(1, 1, self.n_embd)
@@ -129,27 +245,66 @@ class VRWKV_SpatialMix(BaseModule):
 
         return sr, k, v
 
-    def forward(self, x, patch_resolution):
-        B, T, C = x.size()
-        self.device = x.device
+    def forward(self, x, patch_resolution=None):
+        def _inner_forward(x):
+            B, T, C = x.size()
+            self.device = x.device
 
-        sr, k, v = self.jit_func(x, patch_resolution)
-        rwkv = RUN_CUDA(B, T, C, self.spatial_decay / T, self.spatial_first / T, k, v)
-        if self.key_norm is not None:
-            rwkv = self.key_norm(rwkv)
-        rwkv = sr * rwkv
-        rwkv = self.output(rwkv)
-        return rwkv
+            h, w = patch_resolution
+            x = rearrange(x, "b (h w) c -> b c h w", h=h, w=w)
+            x = self.rev(x, True)
+            x = self.dropout(x)  # b c h w
+
+            score = F.softmax(self.gate(x), dim=1)  # b e h w (e=6)
+            score = score.unsqueeze(1)  # b 1 e h w
+
+            x = rearrange(x, "b c h w -> b (h w) c")
+            sr, k, v = self.jit_func(x, patch_resolution)
+
+            k = rearrange(k, "b (h w) c -> b c h w", h=h, w=w)
+            v = rearrange(v, "b (h w) c -> b c h w", h=h, w=w)
+
+            scan_func = [s_hw, s_wh, s_rhw, s_hrw]
+            re_scan_func = [sr_hw, sr_wh, sr_rhw, sr_hrw]
+
+            ks = torch.cat([f(k) for f in scan_func], dim=2)  # b (h w) (c e)
+            vs = torch.cat([f(v) for f in scan_func], dim=2)  # b (h w) (c e)
+
+            expert_outputs = RUN_CUDA(B, T, C * self.num_experts,
+                                      self.spatial_decay / T, self.spatial_first / T, ks, vs)
+            if self.key_norm is not None:
+                expert_outputs = self.key_norm(expert_outputs)
+            expert_outputs = torch.cat([
+                rearrange(re_scan_func[i](expert_outputs[:, :, i * self.attn_sz:(i + 1) * self.attn_sz], h, w),
+                          "b c h w -> b (h w) c") for i in range(self.num_experts)
+            ], dim=2)  # b (h w) (c e)
+            expert_outputs = sr * expert_outputs  # b (h w) (c e)
+            expert_outputs = rearrange(expert_outputs, "b (h w) (c e) -> b c e h w",
+                                       h=h, w=w, c=self.attn_sz, e=self.num_experts)  # b c e h w
+
+            prediction = torch.sum(expert_outputs * score, dim=2)  # b c h w
+            x = self.rev(prediction, False)
+            x = rearrange(x, "b c h w -> b (h w) c")
+
+            x = self.output(x)
+            return x
+
+        if self.with_cp and x.requires_grad:
+            x = cp.checkpoint(_inner_forward, x)
+        else:
+            x = _inner_forward(x)
+        return x
 
 
 class VRWKV_ChannelMix(BaseModule):
     def __init__(self, n_embd, n_layer, layer_id, shift_mode='q_shift',
                  channel_gamma=1 / 4, shift_pixel=1, hidden_rate=4, init_mode='fancy',
-                 key_norm=False):
+                 key_norm=False, with_cp=False):
         super().__init__()
         self.layer_id = layer_id
         self.n_layer = n_layer
         self.n_embd = n_embd
+        self.with_cp = with_cp
         self._init_weights(init_mode)
         self.shift_pixel = shift_pixel
         self.shift_mode = shift_mode
@@ -190,30 +345,36 @@ class VRWKV_ChannelMix(BaseModule):
         else:
             raise NotImplementedError
 
-    def forward(self, x, patch_resolution):
-        if self.shift_pixel > 0:
-            xx = self.shift_func(x, self.shift_pixel, self.channel_gamma, patch_resolution)
-            xk = x * self.spatial_mix_k + xx * (1 - self.spatial_mix_k)
-            xr = x * self.spatial_mix_r + xx * (1 - self.spatial_mix_r)
+    def forward(self, x, patch_resolution=None):
+        def _inner_forward(x):
+            if self.shift_pixel > 0:
+                xx = self.shift_func(x, self.shift_pixel, self.channel_gamma, patch_resolution)
+                xk = x * self.spatial_mix_k + xx * (1 - self.spatial_mix_k)
+                xr = x * self.spatial_mix_r + xx * (1 - self.spatial_mix_r)
+            else:
+                xk = x
+                xr = x
+
+            k = self.key(xk)
+            k = torch.square(torch.relu(k))
+            if self.key_norm is not None:
+                k = self.key_norm(k)
+            kv = self.value(k)
+            x = torch.sigmoid(self.receptance(xr)) * kv
+            return x
+
+        if self.with_cp and x.requires_grad:
+            x = cp.checkpoint(_inner_forward, x)
         else:
-            xk = x
-            xr = x
-
-        k = self.key(xk)
-        k = torch.square(torch.relu(k))
-        if self.key_norm is not None:
-            k = self.key_norm(k)
-        kv = self.value(k)
-
-        rkv = torch.sigmoid(self.receptance(xr)) * kv
-        return rkv
+            x = _inner_forward(x)
+        return x
 
 
 class Block(BaseModule):
     def __init__(self, n_embd, n_layer, layer_id, shift_mode='q_shift',
                  channel_gamma=1 / 4, shift_pixel=1, drop_path=0., hidden_rate=4,
-                 init_mode='fancy', init_values=None, post_norm=False,
-                 key_norm=False, with_cp=False):
+                 init_mode='fancy', init_values=None, post_norm=False, key_norm=False,
+                 with_cp=False):
         super().__init__()
         self.layer_id = layer_id
         self.ln1 = nn.LayerNorm(n_embd)
@@ -225,17 +386,18 @@ class Block(BaseModule):
         self.att = VRWKV_SpatialMix(n_embd, n_layer, layer_id, shift_mode,
                                     channel_gamma, shift_pixel, init_mode,
                                     key_norm=key_norm)
+
         self.ffn = VRWKV_ChannelMix(n_embd, n_layer, layer_id, shift_mode,
                                     channel_gamma, shift_pixel, hidden_rate,
                                     init_mode, key_norm=key_norm)
         self.layer_scale = (init_values is not None)
         self.post_norm = post_norm
         if self.layer_scale:
-            self.gamma1 = nn.Parameter(init_values * torch.ones((n_embd)), requires_grad=True)
-            self.gamma2 = nn.Parameter(init_values * torch.ones((n_embd)), requires_grad=True)
+            self.gamma1 = nn.Parameter(init_values * torch.ones(n_embd), requires_grad=True)
+            self.gamma2 = nn.Parameter(init_values * torch.ones(n_embd), requires_grad=True)
         self.with_cp = with_cp
 
-    def forward(self, x, patch_resolution):
+    def forward(self, x, patch_resolution=None):
         def _inner_forward(x):
             if self.layer_id == 0:
                 x = self.ln0(x)
@@ -263,11 +425,7 @@ class Block(BaseModule):
 
 
 @BACKBONES.register_module()
-class VRWKV(BaseModule):
-    """
-    norm w and norm u
-    """
-
+class VVRWKV(BaseBackbone):
     def __init__(self,
                  img_size=224,
                  patch_size=16,
@@ -279,34 +437,21 @@ class VRWKV(BaseModule):
                  drop_path_rate=0.,
                  channel_gamma=1 / 4,
                  shift_pixel=1,
-                 init_values=None,
                  shift_mode='q_shift',
                  init_mode='fancy',
                  post_norm=False,
                  key_norm=False,
+                 init_values=None,
                  hidden_rate=4,
                  final_norm=True,
                  interpolate_mode='bicubic',
-                 pretrained=None,
                  with_cp=False,
                  init_cfg=None):
-        assert not (init_cfg and pretrained), \
-            'init_cfg and pretrained cannot be specified at the same time'
-        if isinstance(pretrained, str):
-            warnings.warn('DeprecationWarning: pretrained is deprecated, '
-                          'please use "init_cfg" instead')
-            self.init_cfg = dict(type='Pretrained', checkpoint=pretrained)
-        elif pretrained is None:
-            self.init_cfg = init_cfg
-        else:
-            raise TypeError('pretrained must be a str or None')
-        super().__init__(self.init_cfg)
+        super().__init__(init_cfg)
         self.embed_dims = embed_dims
         self.num_extra_tokens = 0
         self.num_layers = depth
         self.drop_path_rate = drop_path_rate
-        logger = get_root_logger()
-        logger.info(f'layer_scale: {init_values is not None}')
 
         self.patch_embed = PatchEmbed(
             in_channels=in_channels,
@@ -351,32 +496,15 @@ class VRWKV(BaseModule):
                 hidden_rate=hidden_rate,
                 drop_path=dpr[i],
                 init_mode=init_mode,
-                init_values=init_values,
                 post_norm=post_norm,
                 key_norm=key_norm,
+                init_values=init_values,
                 with_cp=with_cp
             ))
 
         self.final_norm = final_norm
         if final_norm:
             self.ln1 = nn.LayerNorm(self.embed_dims)
-
-    def init_weights(self):
-        logger = get_root_logger()
-        if self.init_cfg is None:
-            logger.warn(f'No pre-trained weights for '
-                        f'{self.__class__.__name__}, '
-                        f'training start from scratch')
-        else:
-            assert 'checkpoint' in self.init_cfg, f'Only support ' \
-                                                  f'specify `Pretrained` in ' \
-                                                  f'`init_cfg` in ' \
-                                                  f'{self.__class__.__name__} '
-            load_checkpoint(self,
-                            self.init_cfg['checkpoint'], map_location='cpu', logger=logger, strict=False,
-                            revise_keys=[(r'^backbone.', '')])
-            logger.warn(f'Load pre-trained model for '
-                        f'{self.__class__.__name__} from original repo')
 
     def forward(self, x):
         B = x.shape[0]
@@ -405,5 +533,20 @@ class VRWKV(BaseModule):
 
                 out = patch_token
                 outs.append(out)
-
         return tuple(outs)
+
+
+if __name__ == "__main__":
+    model = VVRWKV(
+        img_size=224,
+        patch_size=16,
+        in_channels=3,
+        out_indices=[2, 5, 8, 11],
+        drop_rate=0.,
+        embed_dims=192,
+        depth=12,
+    ).cuda()
+
+    x = torch.randn(1, 3, 224, 224).cuda()
+    out = model(x)
+    print(out[0].shape)
