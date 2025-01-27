c7145e0534455cb96e42ecf83616d67c4796acee
Ling-yunchi <wr020210@gmail.com>
2025-01-27 12:50:45 +0800

    add rsrwkv

--- classification/mmcls_custom/models/backbones/__init__.py
+++ classification/mmcls_custom/models/backbones/__init__.py
@@ -1,5 +1,6 @@
 from .vrwkv import VRWKV
 from .vrwkv6 import VRWKV6
 from .vvrwkv import VVRWKV
+from .rsrwkv import RSRWKV
 
-__all__ = ['VRWKV', 'VRWKV6', 'VVRWKV']
+__all__ = ['VRWKV', 'VRWKV6', 'VVRWKV', 'RSRWKV']
