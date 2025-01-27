c804c66318340f0de8e9298f5fc75f9fd93b171a
Ling-yunchi <wr020210@gmail.com>
2025-01-27 17:01:48 +0800

    update

--- segmentation/mmseg_custom/models/backbones/__init__.py
+++ segmentation/mmseg_custom/models/backbones/__init__.py
@@ -1,4 +1,5 @@
 from .vrwkv_adapter import VRWKV_Adapter
 from .vvrwkv_adapter import VVRWKV_Adapter
+from .rsrwkv import RSRWKV
 
-__all__ = ['VRWKV_Adapter', 'VVRWKV_Adapter']
+__all__ = ['VRWKV_Adapter', 'VVRWKV_Adapter', 'RSRWKV']
