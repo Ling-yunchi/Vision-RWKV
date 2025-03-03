4d62c967de663cb546f93a0180bef8beac0a87bd
Ling-yunchi <wr020210@gmail.com>
2025-03-03 16:48:59 +0800

    update det

--- detection/mmdet_custom/models/backbones/__init__.py
+++ detection/mmdet_custom/models/backbones/__init__.py
@@ -1,3 +1,5 @@
 from .vrwkv_adapter import VRWKV_Adapter
+from .vvrwkv_adapter import VVRWKV_Adapter
+from .vit_adapter import ViT_Adapter
 
-__all__ = ['VRWKV_Adapter']
+__all__ = ['VRWKV_Adapter', 'VVRWKV_Adapter', 'ViT_Adapter']
